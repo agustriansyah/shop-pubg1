f0cc66f10690639ba80bffc502be2fdcc3b53271
agustriansyah <agustricip@gmail.com>
2024-08-30 08:52:26 +0800

    ubah all

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const sideBar = document.querySelector(".sidebar");
 const nav = document.querySelector("nav")
-const container = document.querySelector(".container");
+const section = document.querySelector("section");
 const menuBtn = document.querySelector(".fa-bars");
 
 menuBtn.addEventListener("click", () => {
     sideBar.classList.toggle("active")
     nav.classList.toggle("active")
-    container.classList.toggle("active");
+    section.classList.toggle("active");
 })
